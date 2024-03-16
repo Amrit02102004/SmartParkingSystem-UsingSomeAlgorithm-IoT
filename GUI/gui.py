685be904875f18c8596b81631be4bb5247f7e614
PAGADALA MOKSHAGNA <121112283+PAGADALA-MOKSHAGNA@users.noreply.github.com>
2024-03-16 22:56:59 +0530

    created intial gui for the project using python - kivy library.

---
+++ GUI/gui.py
@@ -0,0 +1,16 @@
+import kivy
+from kivy.metrics import dp
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App
+
+class Menu_bar(BoxLayout):
+    pass
+
+class MainFrame(BoxLayout):
+    pass
+
+class test_gui(App):
+    pass
+
+if __name__ == "__main__":
+    test_gui().run()
